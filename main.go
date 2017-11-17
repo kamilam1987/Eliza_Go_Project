9cf55727ec202d0d83e0193433b1be3101912577
Kamila Michel <kamilam1987@gmail.com>
2017-11-17 17:28:32 +0000

    set static site

---
+++ main.go
@@ -0,0 +1,18 @@
+//Author: Kamila Michel
+//Eliza project
+//This project contains a chatbot web application in Go. User will be able to visit the web app throught their browser, type in some sentences and web app will reply to it.
+
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	//code adapted from:https://stackoverflow.com/questions/26559557/how-do-you-serve-a-static-html-file-using-a-go-web-server
+	// FileServe serves index.html from static folder
+	//http.Handle function tells the http package to handle all requests from static folder (index.html)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":8088", nil)
+
+} //End of function main
